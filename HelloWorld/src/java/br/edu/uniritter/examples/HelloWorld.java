7b85365e5e9770b699315a5654dd421f059736b2
Regis Drunn Pereira <rdruper@gmail.com>
2012-05-10 15:27:22 -0300

    Signed-off-by: Regis Drunn Pereira <rdruper@gmail.com>

---
+++ HelloWorld/src/java/br/edu/uniritter/examples/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.uniritter.examples;
+
+/**
+ *
+ * @author alu201230433
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World rá");
+    }
+    
+}
